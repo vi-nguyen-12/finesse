4bd829269ad9c99b448704f09f0b8492df1a5a15
Vi <vienne@labs196.com>
2023-05-05 16:36:20 -0500

    size words for navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,16 +9,31 @@ const Navbar = () => {
   const windowSize = useWindowSize();
   return (
     <div className="navbar-container d-flex align-items-center justify-content-between p-5">
-      <div>
-        <GoThreeBars size={70} />
-        <AiOutlineSearch size={70} />
+      <div className="d-flex">
+        <GoThreeBars
+          className="d-block me-5"
+          size={windowSize === "sm" ? 40 : windowSize === "md" ? 50 : 70}
+        />
+        <AiOutlineSearch
+          size={windowSize === "sm" ? 40 : windowSize === "md" ? 50 : 70}
+          className={`${windowSize !== "sm" && "ml-3"}`}
+        />
       </div>
 
       <div>
-        <img src={barCode} style={{ height: "80px" }} />
+        <img
+          src={barCode}
+          style={{ height: `${windowSize === "sm" ? "60px" : "80px"}` }}
+        />
         <h3 className="mt-0">FINESSE</h3>
       </div>
-      <button className=" bg-transparent border-0 fs-6">CART (1)</button>
+      <button
+        className={`bg-transparent border-0  ${
+          windowSize === "sm" ? "fs-18" : "fs-24"
+        }`}
+      >
+        CART (1)
+      </button>
     </div>
   );
 };
