5fd03a94b5217b0c4d4d83c02926095ed738c25c
Vi <vienne@labs196.com>
2023-05-06 13:34:58 -0500

    animation on card

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,23 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import "./ProductCard.css";
 
 const ProductCard = ({ product }) => {
   const navigate = useNavigate();
+  const [isHovering, setIsHovering] = useState(false);
   const handleClick = () => {
     navigate(`/?id=${product._id}`);
     window.scrollTo(0, 0);
+  };
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+  const handleMouseOut = () => {
+    setIsHovering(false);
   };
   return (
     <div
       className="w-100 mt-5"
       style={{ cursor: "pointer" }}
-      onClick={handleClick}
+      onMouseOver={handleMouseOver}
+      onMouseOut={handleMouseOut}
     >
-      <img
-        src={product.images[0]}
-        className="rounded-4"
-        style={{ width: "100%" }}
-      />
+      <div className="position-relative">
+        <div className="wrapper">
+          <img src={product.images[0]} className="rounded-8 w-100" />
+        </div>
+        {isHovering && (
+          <div className="hover-container">
+            <div className="hover-container-bg"></div>
+            <div className="hover-container-btn">
+              <button onClick={handleClick}>View</button>
+            </div>
+          </div>
+        )}
+      </div>
 
       <h5 className="fw-600 mt-3 fs-16">{product.name}</h5>
       <p className="fw-600 secondary-color">${product.price}</p>
